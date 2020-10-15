f8f961c2f84dc21f7edc3f82ca6ec4913918c65e
nakazono0424 <nakazono2019@s.okayama-u.ac.jp>
2020-10-15 12:48:12 +0900

    Add collision method and reset method

--- src/game_state.rs
+++ src/game_state.rs
@@ -18,5 +18,23 @@ impl GameState {
         self.world
             .player2
             .update(time, self.world.width, self.world.height);
+        self.collisions();
+    }
+
+    pub fn collisions(&mut self) {
+        let x_distance: f64 = self.world.player1.x() - self.world.player2.x();
+        let y_distance: f64 = self.world.player1.y() - self.world.player2.y();
+        if (x_distance * x_distance + y_distance * y_distance) < 40.0 * 40.0 {
+            self.reset();
+        }
+    }
+
+    pub fn reset(&mut self) {
+        // Reset player position
+        *self.world.player1.x_mut() = self.world.width - 20.0;
+        *self.world.player1.y_mut() = self.world.height - 20.0;
+
+        *self.world.player2.x_mut() = 20.0;
+        *self.world.player2.y_mut() = 20.0;
     }
 }
